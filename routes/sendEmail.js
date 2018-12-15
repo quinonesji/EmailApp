831856c43410b30806e413381bc843ec6a06115b
Nachito <Nachito@DESKTOP-2V3QTRO>
2018-12-15 00:19:04 -0500

    hello

--- routes/sendEmail.js
+++ routes/sendEmail.js
@@ -40,8 +40,8 @@ router.post('/', function(req, res, next) {
 
     } else {
         // Object is NOT empty
-        console.log(req.originalUrl);
-        if(req.originalUrl  === "resources") {
+        console.log(req.body.designapplication);
+        if(req.body.designapplication === "designform") {
             //req is coming from resources page
             //var resObj = SendEmail(myObj, "/views/designIndex.pug");
             var data = jade.renderFile(process.cwd() + '/views/design.jade', myObj)
